83deaa124726f1ea0a0843a0a9aa637adf95dc26
Husanboy <71025836+Husan077@users.noreply.github.com>
2023-06-08 13:43:06 +0500

    web

--- resources/views/admin/classes/header.blade.php
+++ resources/views/admin/classes/header.blade.php
@@ -18,6 +18,15 @@
                         {{ auth()->user()->name ?? '' }}
                     </div>
                     <a class="dropdown-item" href="{{ route('logout') }}">Выйти</a>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-dropdown-link :href="route('logout')"
+                                         onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-dropdown-link>
+                    </form>
                 </div>
             </div>
         </div>
